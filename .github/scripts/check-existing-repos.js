50912b9c3b30ae3eb26ad1c58001a01792bb50f0
Nick Alteen <ncalteen@github.com>
2023-04-19 11:20:42 -0400

    Switch to async/await handling

--- .github/scripts/check-existing-repos.js
+++ .github/scripts/check-existing-repos.js
@@ -1,41 +1,34 @@
-const fs = require('fs')
-
 module.exports = async ({github, context, options, core}) => {
-  options.repositories.forEach(repository => {
-    console.log(
-      `Checking if ${options.targetOrganization}/${
-        repository.split(',')[1]
-      } exists...`,
-    )
+  const duplicates = []
 
-    github.rest.repos
-      .get({
+  options.repositories.forEach(async repository => {
+    try {
+      await github.rest.repos.get({
         owner: options.targetOrganization,
         repo: repository.split(',')[1],
       })
-      .then(() => {
-        let commentBody = `:no_entry: **Validation failed.** One or more repositories already exist:
-    
-        \`\`\`plain
-        ${repository}
-        \`\`\`
-
-        Please remove the duplicates from your issue body and try again.
-        `
-
-        github.rest.issues.createComment({
-          issue_number: context.issue.number,
-          owner: context.repo.owner,
-          repo: context.repo.repo,
-          commentBody,
-        })
-
-        core.setFailed(
-          `Validation failed. One or more repositories already exist.`,
-        )
-      })
-      .catch(error => {
-        // Do nothing
-      })
+    } catch (error) {
+      duplicates.push(repository)
+    }
   })
+
+  if (duplicates.length !== 0) {
+    let commentBody = `:no_entry: **Validation failed.** One or more repositories already exist:
+
+    \`\`\`plain
+    ${duplicates.join('\n')}
+    \`\`\`
+
+    Please remove the duplicates from your issue body and try again.
+    `
+
+    await github.rest.issues.createComment({
+      issue_number: context.issue.number,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      commentBody,
+    })
+
+    core.setFailed(`Validation failed. One or more repositories already exist.`)
+  }
 }
