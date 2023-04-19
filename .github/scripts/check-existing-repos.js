806592b7bbf1d12e00cbb8a9d75709c0b31e7f4b
Nick Alteen <ncalteen@github.com>
2023-04-19 11:09:16 -0400

    Move async repo check handling

--- .github/scripts/check-existing-repos.js
+++ .github/scripts/check-existing-repos.js
@@ -1,48 +1,41 @@
 const fs = require('fs')
 
 module.exports = async ({github, context, options, core}) => {
-  const duplicates = []
-
   options.repositories.forEach(repository => {
     console.log(
       `Checking if ${options.targetOrganization}/${
         repository.split(',')[1]
       } exists...`,
     )
+
     github.rest.repos
       .get({
         owner: options.targetOrganization,
         repo: repository.split(',')[1],
       })
-      .then(response => {
-        duplicates.push(repository)
-        console.log(duplicates)
+      .then(() => {
+        let commentBody = `:no_entry: **Validation failed.** One or more repositories already exist:
+    
+        \`\`\`plain
+        ${repository}
+        \`\`\`
+
+        Please remove the duplicates from your issue body and try again.
+        `
+
+        github.rest.issues.createComment({
+          issue_number: context.issue.number,
+          owner: context.repo.owner,
+          repo: context.repo.repo,
+          commentBody,
+        })
+
+        core.setFailed(
+          `Validation failed. One or more repositories already exist.`,
+        )
       })
       .catch(error => {
         // Do nothing
       })
   })
-
-  console.log(duplicates)
-  console.log(duplicates.length)
-
-  if (duplicates.length > 0) {
-    let commentBody = `:no_entry: **Validation failed.** One or more repositories already exist:
-    
-    \`\`\`plain
-    ${duplicates.join('\n')}
-    \`\`\`
-
-    Please remove the duplicates from your issue body and try again.
-    `
-
-    await github.rest.issues.createComment({
-      issue_number: context.issue.number,
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      commentBody,
-    })
-
-    core.setFailed(`Validation failed. One or more repositories already exist.`)
-  }
 }
