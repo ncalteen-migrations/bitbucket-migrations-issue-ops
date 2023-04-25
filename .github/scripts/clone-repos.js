124be3616a9a226d10fd58a092a05d6d22defe06
Nick Alteen <ncalteen@github.com>
2023-04-25 11:28:51 -0400

    Fix repo paths

--- .github/scripts/clone-repos.js
+++ .github/scripts/clone-repos.js
@@ -4,13 +4,15 @@
 module.exports = async ({github, context, core, options, exec}) => {
   let targetRepositoryUrl
   let sourceRepositoryUrl
+  let repo
 
   // Create a temporary directory to clone the source repositories
   await exec.exec(`mkdir ${options.temp}/repository`)
 
   options.repositories.forEach(repository => {
-    sourceRepositoryUrl = `https://x-token-auth:${process.env.BITBUCKET_CLOUD_ADMIN_TOKEN}@bitbucket.org/${process.env.BITBUCKET_CLOUD_WORKSPACE}/${repository}.git`
-    targetRepositoryUrl = `https://${process.env.GHEC_ADMIN_TOKEN}@github.com/${options.targetOrganization}/${repository}.git`
+    repo = repository.split(',')
+    sourceRepositoryUrl = `https://x-token-auth:${process.env.BITBUCKET_CLOUD_ADMIN_TOKEN}@bitbucket.org/${process.env.BITBUCKET_CLOUD_WORKSPACE}/${repo}.git`
+    targetRepositoryUrl = `https://${process.env.GHEC_ADMIN_TOKEN}@github.com/${options.targetOrganization}/${repo}.git`
 
     // Clone the source repository
     exec
@@ -18,19 +20,19 @@ module.exports = async ({github, context, core, options, exec}) => {
         cwd: `${options.temp}`,
       })
       .then(() => {
-        core.info(`Repository cloned: ${repository}`)
+        core.info(`Repository cloned: ${repo}`)
 
         // Push the source repository to the target repository
         exec
           .exec(`git push --mirror ${targetRepositoryUrl}`, [], {
-            cwd: `${options.temp}/${repository}`,
+            cwd: `${options.temp}/${repo}`,
           })
           .then(async () => {
-            core.info(`Repository mirrored: ${repository}`)
+            core.info(`Repository mirrored: ${repo}`)
 
             // Empty the temporary directory
             exec.exec(`rm -rf .`, [], {
-              cwd: `${options.temp}/${repository}`,
+              cwd: `${options.temp}/${repo}`,
             })
           })
       })
